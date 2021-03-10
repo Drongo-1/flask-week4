d1be05e213cb7649e13240c9b61c903058ab2109
Drongo-1 <Drongo-1>
2021-03-10 09:10:55 +0300

    edited files

--- app/dict.py
+++ app/dict.py
@@ -1,7 +1,7 @@
 import random
 from flask import (render_template, url_for, flash,
                    redirect, request, abort, Blueprint)
-def quitess():
+def quotes():
     dict1= {1:{'author': 'Ngugidavid', 'title': 'random', 'quote':'This is a random quote'},
                    2:{'author': 'Ngugidavid2', 'title': 'random', 'quote':'This is a random quote'}} 
     res = key, val = random.choice(list(dict1.items())) 
