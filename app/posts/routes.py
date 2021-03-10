112ff5cc36d7b4942c90d842e2b8e48be7bc6901
Drongo-1 <Drongo-1>
2021-03-10 08:38:16 +0300

    edited files

--- app/posts/routes.py
+++ app/posts/routes.py
@@ -65,10 +65,3 @@ def delete_post(post_id):
     return redirect(url_for("main.home"))
 
 
-def quotes():
-    dict1= {1:{'author': 'Ngugidavid', 'title': 'random', 'quote':'This is a random quote'},
-                   2:{'author': 'Ngugidavid2', 'title': 'random', 'quote':'This is a random quote'}} 
-    res = key, val = random.choice(list(dict1.items())) 
-    # print("The random pair is : " + str(res)) 
-    newauthor=str(val['author']) 
-    return render_template('quotes.html', newauthor=newauthor, val=val)
