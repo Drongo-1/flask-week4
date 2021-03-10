b654126ac78e1972c53cbb52bcba0cb78f6731dd
Drongo-1 <Drongo-1>
2021-03-10 14:53:40 +0300

    edited files

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
+import random
+import render_template
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
