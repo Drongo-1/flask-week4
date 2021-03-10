543f1c862f4f7b41996486cd7da3a3812da7bcf5
Drongo-1 <Drongo-1>
2021-03-10 07:37:48 +0300

    edited files

--- app/dict.py
+++ app/dict.py
@@ -1,14 +1,9 @@
+import random
+from flask import (render_template, url_for, flash,
+                   redirect, request, abort, Blueprint)
 class Dict1():
     dict[posts] = {'post_id':1,'author': 'Ngugidavid', 'title': 'random', 'quote':'This is a random quote'}
-    return
     res = key, val = random.choice(list(test_dict.items())) 
     # print("The random pair is : " + str(res)) 
-    print(str(val['author'])) 
-
-
-
-//method 2
-res = test_dict.popitem() 
-print("The random pair is : " + str(res)) 
-
-
+    newauthor=(str(val['author'])) 
+    return render_template('layout.html', newauthor=newauthor)
